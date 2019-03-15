d7625c38e0e11001cf57d15cc2b151e91387d2a7
Martin <martin.soethe@uni-flensburg.de>
2019-03-15 16:26:39 +0100

    Update build.py
    Do not rely on pathlib

--- build.py
+++ build.py
@@ -1,86 +1,84 @@
 # -*- coding: utf-8 -*-
+""" """
 
 import shutil
 import zipfile
-import pathlib
 import os
 
 import pandas as pd
 from oemof.tabular.datapackage import building, processing
-
-from oemof.tabular.datapackage.processing import write_elements, write_sequences
+from oemof.tabular.datapackage.processing import \
+    write_elements, write_sequences
 
 from xlrd import XLRDError
 
+# path handling
+dpkg = './datapackages'
+base = os.path.join(dpkg, 'SQ')
 
-# recreate datapackages/SQ folder
-pkpath = pathlib.Path('datapackages/SQ')
-if pkpath.parent.is_dir():
-    shutil.rmtree(pkpath.parent)
-pkpath.mkdir(parents=True)
+if os.path.exists(dpkg):
+    shutil.rmtree(dpkg)
 
-# download and unzip SQ datapackage
-pkurl = 'https://github.com/ZNES-datapackages/Status-quo-2015/releases/download/v0.1-alpha/Status-quo-2015.zip'
+os.mkdir(dpkg)
+
+# unzip reference datapackage
 zipfile.ZipFile(
-    building.download_data(pkurl, directory=pkpath), 'r'
-).extractall(pkpath)
+    building.download_data(
+        'https://github.com/ZNES-datapackages/Status-quo-2015/releases/'
+        'download/v0.1-alpha/Status-quo-2015.zip',
+        directory=dpkg), 'r').extractall(base)
 
+# load archived data
+xls = './archive/data.xls'
+datapackages = pd.read_excel(xls, index_col='identifier')
 
-scenarios = pd.read_excel(os.path.join('archive', 'scenarios.xls'), index_col='identifier').index.values
+for pk in datapackages.index:
 
-xls = 'archive/scenarios.xls'
+    # declare paths
+    path = os.path.join(dpkg, pk)
+    epath = os.path.join(path, 'data', 'elements')
+    spath = os.path.join(path, 'data', 'sequences')
 
-for sc in scenarios:
+    # copy datapackage
+    processing.copy_datapackage(os.path.join(base, 'datapackage.json'), path)
 
-    path = pathlib.Path(pkpath.parent, sc)
-    #path.mkdir()
-
-    processing.copy_datapackage(
-        os.path.join(pkpath, 'datapackage.json'), path)
-
-    elements_path = os.path.join(path, 'data', 'elements')
-    sequences_path = os.path.join(path, 'data', 'sequences')
-
+    # fetch timeseries data
     try:
 
-        timeseries = pd.read_excel(xls, sheet_name='timeseries' + '-' + sc)
-        timeseries.set_index(pd.date_range('2015-01-01 00:00:00', '2015-12-31 23:00:00', freq='H'), inplace=True)
-        timeseries = timeseries['Sum']
+        # TODO: parse dates from xls file
+        timesteps = pd.date_range('2015-01-01 00:00:00', '2015-12-31 23:00:00', freq='H')
 
-        sequence = timeseries.apply(lambda z: z if z > 0 else 0)
-        sequence.name = 'BO-plus-profile'
+        ts = pd.read_excel(
+                xls, sheet_name='timeseries' + '-' + pk
+            ).set_index(timesteps)['Sum']
 
-        element = {
-            'BO-plus':
-            {
-                'bus': 'DE-electricity',
-                'capacity': 1,
-                'profile': 'BO-plus-profile',
-                'type': 'volatile'
-            }
-        }
+        positive = ts.apply(lambda x: x if x > 0 else 0)
+        positive.name = 'BO-plus-profile'
 
-        write_elements(
-            'volatile.csv', pd.DataFrame(element).T, directory=elements_path)
-        write_sequences(
-            'volatile_profile.csv', sequence, directory=sequences_path)
+        negative = ts.apply(lambda x: x if x < 0 else 0).abs()
+        negative.name = 'BO-minus-profile'
 
-        sequence = timeseries.apply(lambda y: y if y < 0 else 0).abs()
-        sequence.name = 'BO-minus-profile'
+        # positive values represent a net generation for the macro-system
+        header = ['bus', 'capacity', 'profile', 'type']
+        data = ['DE-electricity', 1, 'BO-plus-profile', 'volatile']
 
-        element = {
-            'BO-minus':
-            {
-                'bus': 'DE-electricity',
-                'amount': 1,
-                'profile': 'BO-minus-profile',
-                'type': 'load'
-            }
-        }
+        element = pd.DataFrame({
+            'BO-plus': dict(zip(header, data))
+        }).T
 
-        write_elements(
-            'load.csv', pd.DataFrame(element).T, directory=elements_path)
-        write_sequences('load_profile.csv', sequence, directory=sequences_path)
+        write_elements('volatile.csv', element, directory=epath)
+        write_sequences('volatile_profile.csv', positive, directory=spath)
+
+        # negative values represent a net demand for the macro-system
+        header = ['bus', 'amount', 'profile', 'type']
+        data = ['DE-electricity', 1, 'BO-minus-profile', 'load']
+
+        element = pd.DataFrame({
+            'BO-minus': dict(zip(header, data))
+        }).T
+
+        write_elements('load.csv', element, directory=epath)
+        write_sequences('load_profile.csv', negative, directory=spath)
 
     except XLRDError as e:
         pass
